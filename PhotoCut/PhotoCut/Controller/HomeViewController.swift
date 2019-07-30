13fd2170d18a2405ea1a1edfc8305561d3c59ef7
Kumar Lav <kumarstslav@gmail.com>
2019-07-30 10:53:56 +0530

    my first commit PhotoCut

---
+++ PhotoCut/PhotoCut/Controller/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  PhotoCut
+//
+//  Created by apple on 29/07/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
